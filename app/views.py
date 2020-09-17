e2782f5364fa5ece7f557144a781d2c0ecb2e7f3
Abdul Basith <abdulbasithCOC@gmail.com>
2020-09-18 00:59:09 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -52,7 +52,7 @@ def developers(request):
 def scrape_wikipedia(qry):
     import wikipedia
 
-    list_returned=wikipedia.search("what the hell is this python")  #return a list
+    list_returned=wikipedia.search(qry)  #return a list
     #print(list_returned[0])
     #print(wikipedia.summary(list_returned[0]))
     return wikipedia.summary(list_returned[0])
