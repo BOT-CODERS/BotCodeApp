8d525644f13779bc94b4a6ae6e12455f2389a414
RutujaD182 <dhanawaderutuja1002@gmail.com>
2020-09-19 23:27:07 +0530

    Added frontend

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
     path('search/',views.search,name='search'),
     path('developers/',views.developers,name="developers"),
     path('add',views.add,name='add'),
+    path('404/',views.error,name='error'),
 ]
