a334144701b8c9cedad51746ae31b4ef54e50d8c
Apoorve Goyal <apoorve73@gmail.com>
2020-10-01 16:51:21 +0530

    Update Wiki

--- app/wiki_answer.py
+++ app/wiki_answer.py
@@ -1,8 +1,22 @@
+import wikipediaapi
+from . import general_answer as gen
+import platform
 # wikipedia scrape begins
 def scrape_wikipedia(qry):
-    import wikipedia
+	wiki_wiki = wikipediaapi.Wikipedia('en')
+	page_py = wiki_wiki.page(qry)
+	
+	if page_py.exists():
+		return page_py.summary
+		
+	else:
+		OS = platform.system()
+		if(OS == "Linux") :
+			Driver_Path = "app/Linux/chromedriver"
 
-    list_returned = wikipedia.search(qry)  # return a list
-    # print(list_returned[0])
-    # print(wikipedia.summary(list_returned[0]))
-    return wikipedia.summary(list_returned[0])
+		elif(OS == "Windows"):
+			Driver_Path = "app/Win/chromedriver"
+
+		general_res = gen.general_answer(qry, Driver_Path)
+		return general_res
+	
