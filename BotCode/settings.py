8fdd86b74baa1f5482a8460eab1d2b3fb10b8899
Apoorve Goyal <apoorve73@gmail.com>
2020-10-03 20:45:39 +0530

    Add STATIC

--- BotCode/settings.py
+++ BotCode/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'app/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
