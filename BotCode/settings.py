ddfefd8e7c4c30a60a51cd470578277f9d826ec0
Apoorve Goyal <apoorve73@gmail.com>
2020-10-03 20:34:22 +0530

    Add static root

--- BotCode/settings.py
+++ BotCode/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #to work with static files,we've to specify the path to look for static files here
